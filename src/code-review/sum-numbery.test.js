6eb16a65a259a3cd313c08de8139213d24e32938
Pallavi Sarwar <pallavi.sarwar@gmail.com>
2024-04-26 14:28:19 +0200

    fix and disable some lines

--- src/code-review/sum-numbery.test.js
+++ src/code-review/sum-numbery.test.js
@@ -1,6 +1,7 @@
 // #todo
-// eslint-disable-next-line strict, lines-around-directive
+
 'use strict';
+
 /**
  * Sums all numbery strings in an array of strings.
  * Returns 0 if the array is empty.
@@ -9,6 +10,7 @@
  * @returns {number} The sum of all numbery strings.
  */
 
+// -------- your solutions --------
 const sumNumberyStrings = (arr) => {
   let sum = 0;
   for (const str of arr) {
@@ -20,9 +22,21 @@ const sumNumberyStrings = (arr) => {
   }
   return sum;
 };
-for (const solution of [
-  sumNumberyStrings, // Updated function name to match the actual function
-]) {
+
+const mapFilterReduce = (arr) => {
+  const isNotNaN = (entry) => !Number.isNaN(entry);
+  const sumNumbers = (acc, next) => acc + next;
+  const castToNumber = (entry) => Number(entry);
+
+  const sumOfNumberies = arr
+    .map(castToNumber)
+    .filter(isNotNaN)
+    .reduce(sumNumbers, 0);
+
+  return sumOfNumberies;
+};
+
+for (const solution of [sumNumberyStrings, mapFilterReduce]) {
   describe(`${solution.name}: sumNumberyStrings`, () => {
     describe('checking different test cases', () => {
       it('returns 0 for an empty array', () => {
@@ -42,3 +56,10 @@ for (const solution of [
     });
   });
 }
+
+// minified solution for testing your tests
+
+// prettier-ignore
+// eslint-disable-next-line max-len
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions, jsdoc/require-jsdoc, no-unused-vars, quotes, eqeqeq, no-shadow, arrow-parens, no-implicit-coercion
+function secretSolution(a) { if (!Array.isArray(a)) { throw new TypeError("arr is not an array"); } const b = a.some(a => typeof a != "string"); if (b) { throw new TypeError("arr contains non-strings"); } const c = a.map(a => +a).filter(a => !Number.isNaN(a)).reduce((a, b) => a + b, 0); return c; }
